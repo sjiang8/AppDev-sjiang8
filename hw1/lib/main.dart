9f1a565b0fdda5a6f6bbfea172bebdb24cc4597d
sjiang8 <56941809+sjiang8@users.noreply.github.com>
2022-06-17 23:45:57 -0400

    first commit

---
+++ hw1/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random(); // Add this line.
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter'),
+        ),
+        body: Center(
+          child: Text(wordPair.asPascalCase), //Changed text
+        ),
+      ),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[]; // NEW
+  final _biggerFont = const TextStyle(fontSize: 18); // NEW
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
